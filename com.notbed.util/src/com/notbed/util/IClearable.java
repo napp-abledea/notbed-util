38d1aa7204912d1cbce847b5e0398c356a1ec5b4
al <al@mini.notbed.com>
2013-11-08 19:19:57 +0200

    interface goodness

---
+++ com.notbed.util/src/com/notbed/util/IClearable.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package com.notbed.util;
+
+/**
+ * @author Alexandru Bledea
+ * @since Nov 7, 2013
+ */
+public interface IClearable {
+
+	/**
+	 *
+	 */
+	void clear();
+}
---
+++ com.notbed.util/src/com/notbed/util/IDroppableCache.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+package com.notbed.util;
+
+/**
+ * @author Alexandru Bledea
+ * @since Nov 7, 2013
+ */
+public interface IDroppableCache {
+
+	/**
+	 *
+	 */
+	void dropCache();
+
+}
